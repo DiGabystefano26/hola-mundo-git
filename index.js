7a463f39bd46abc674bbe5d9cc2cedb6d2fb1cbc
Gabriela Distefano <gabrieladistefano1@gmail.com>
2022-08-18 14:31:41 -0400

    Se creo la funcion calcularEdad()

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-
 let nombre = "Jung Hoseok";
-let edad = 28;
+let edad = calcularEdad(2022);
 console.log("hola " + nombre);
 console.log(nombre + "tiene "+ edad + "años");
+
+function calcularEdad(AnioNacimiento){
+    const fechaActual = new Date();
+    const AnioActual = fechaActual.getFullYear();
+    const edad = AnioActual - AnioNacimiento;
+    return edad;
+}
