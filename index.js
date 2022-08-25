77ec16be115d40a9bcf0f6d777c38e8d184f1b8d
Gabriela Distefano <gabrieladistefano1@gmail.com>
2022-08-25 14:09:42 -0400

    se agrego la funcion calcular salario

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ let nombre = "Jung Hoseok";
 let edad = calcularEdad(1994);
 console.log("hola " + nombre);
 console.log(nombre + "tiene "+ edad + "años");
+console.log("Se le pagara: "+calcularSalario(horasTrabajadas, 30000));
 
 function calcularEdad(AnioNacimiento){
     return new Date().getFullYear() - AnioNacimiento;
+}
+
+function calcularSalario(horasTrabajadas, montoPorHora){
+    return horasTrabajadas * montoPorHora;
 }
